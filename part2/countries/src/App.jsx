c6effa799b7dad4f407320be41e9fb14336850c4
amun1303 <pigbanguy@gmail.com>
2024-11-10 20:03:42 +0200

    part 2 Done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const CountryList = ({ countries, onShowCountry }) => {
+  if (countries.length > 10) {
+    return <p>too many matches, specify another filter</p>;
+  } else if (countries.length > 1) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.cca3}>
+            {country.name.common + " "}
+            <button onClick={() => onShowCountry(country)}>Show</button>
+          </li>
+        ))}
+      </ul>
+    );
+  } else if (countries.length === 1) {
+    return <CountryDetails country={countries[0]} />;
+  }
+};
+
+const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+  const capital = country.capital[0];
+  console.log(apiKey);
+
+  axios
+    .get(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        capital +
+        "&units=metric&appid=" +
+        apiKey
+    )
+    .then((response) => {
+      setWeather(response.data);
+    });
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital[0]}</p>
+      <p>area {country.area} km2</p>
+      <h3>languages</h3>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
+      {weather && (
+        <div>
+          <h3>Weather in {capital}</h3>
+          <p>temperature: {weather.main.temp} °C</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          />
+          <p>wind: {weather.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
+
+  const onSearchChange = (event) => {
+    const query = event.target.value;
+    setSearch(query);
+
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredCountries(filtered);
+    setSelectedCountry(null);
+  };
+
+  const onShowCountry = (country) => {
+    setSelectedCountry(country);
+    setFilteredCountries([country]);
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        find countries: <input value={search} onChange={onSearchChange} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {selectedCountry ? (
+        <CountryDetails country={selectedCountry} />
+      ) : (
+        <CountryList
+          countries={filteredCountries}
+          onShowCountry={onShowCountry}
+        />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
