f54518c557aa570caf831fa4a2f70b136ee1ae07
amun1303 <pigbanguy@gmail.com>
2024-11-22 13:27:51 +0200

    change port

--- part3/phonebook_be/index.js
+++ part3/phonebook_be/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 
 morgan.token('body', (request) => {
     return JSON.stringify(request.body);
@@ -80,7 +82,7 @@ app.get("/info", (request, response) => {
     response.send("phonebook has " + numberOfEntries + " people<br/>" + currentTime);
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
 })
